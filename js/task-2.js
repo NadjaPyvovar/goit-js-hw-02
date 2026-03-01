91661fb7eb9eb6a575baa906ba5b4953b6d9b96c
Nadja <nadja.pyvovar@gmail.com>
2026-03-01 20:16:25 +0200

    finish hw 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,18 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    return message.slice(0, maxLength) + '...';
+  }
 }
 
-console.log(getShippingMessage('Australia', 120, 50));
-console.log(getShippingMessage('Germany', 80, 20));
-console.log(getShippingMessage('Sweden', 100, 20));
+console.log(formatMessage('Curabitur ligula sapien', 16));
+console.log(formatMessage('Curabitur ligula sapien', 23));
+console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
 
-// // Short version - arrow function returning automatically the result without "return" (i.e. const functionName = (...parameters) => ):
-
-// const getShippingMessage = (country, price, deliveryFee) =>
-//   `Shipping to ${country} will cost ${price + deliveryFee} credits`;
-
-// console.log(getShippingMessage('Australia', 120, 50));
-// console.log(getShippingMessage('Germany', 80, 20));
-// console.log(getShippingMessage('Sweden', 100, 20));
+////using ternary operator and arrow function (syntax based on the example const sum = (num1, num2) => {return num1 + num2;}, where const sum = : assign the function to a variable, (num1, num2): parameters, => : the arrow separating parameters from the function body, {return num1 + num2}: function body)
+//const formatMessage = (message, maxLength) =>
+//message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,21 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-
-  const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
-  return totalWidth;
+function checkForSpam(message) {
+  const modifiedMessage = message.toLowerCase();
+  if (modifiedMessage.includes('spam') || modifiedMessage.includes('sale')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
 
-//Short version - arrow function:
-
-// const getElementWidth = (content, padding, border) =>
-//   parseFloat(content) + 2 * (parseFloat(padding) + parseFloat(border));
-
-// console.log(getElementWidth('50px', '8px', '4px'));
-// console.log(getElementWidth('60px', '12px', '8.5px'));
-// console.log(getElementWidth('200px', '0px', '0px'));
+//using arrow function:
+//const checkForSpam = (message) => {
+// const modifiedMessage = message.toLowerCase();
+//return modifiedMessage.includes("spam") || modifiedMessage.includes("sale");}
